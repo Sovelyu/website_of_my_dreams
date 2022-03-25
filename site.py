e8524c39e46fcf9776bddb84a30a8adf5c5b4479
Анна <kotkovaann00@mail.ru>
2022-03-25 14:25:19 +0300

    .

--- site.py
+++ site.py
@@ -15,7 +15,6 @@ def obsidian():
     return '''<!doctype html>
                 <html lang="en">
                   <head>
-
                     <meta charset="utf-8">
                     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
                     <link rel="stylesheet"
